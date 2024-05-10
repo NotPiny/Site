acbcb7bc5ab7b709af1e3b1960e3a6c461c77884
NotPiny <103984634+NotPiny@users.noreply.github.com>
2024-05-10 19:27:59 +0100

    Just some little modifications to the api

--- src/routes/api/get-ip/+server.ts
+++ src/routes/api/get-ip/+server.ts
@@ -2,7 +2,7 @@ import type { RequestEvent } from "@sveltejs/kit";
 import axios from 'axios';
 
 export async function GET(req: RequestEvent) {
-    const ip = req.url.searchParams.get('ip');
+    const ip = req.url.searchParams.get('ip') ?? req.getClientAddress();
 
     const response = await axios.get(`https://api.ip2location.io/?key=${process.env.IPTOLOCATION_KEY}&ip=${ip}`);
 
